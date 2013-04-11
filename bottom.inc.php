b689121fb1f93deb7a5c66841d2f7fd68c6261c3
samrat <samrat@Samrat>
2013-04-11 10:17:08 +0600

    footer is including first time

---
+++ bottom.inc.php
@@ -0,0 +1,16 @@
+    <div class="navbar navbar-fixed-bottom">
+        <div class="navbar-inner">
+            <div class="row-fluid">
+                <p>StepOneWMS v 0.1 &copy;  <?php echo date('Y'); ?>. Stepone Group Limited</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="<?php echo CONTENTPATH; ?>/foo-drawer/js/jquery-1.8.3.min.js"></script>
+<script src="<?php echo CONTENTPATH; ?>/foo-drawer/js/bootstrap.min.js"></script>
+<script src="<?php echo CONTENTPATH; ?>/foo-drawer/js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+<script src="<?php echo CONTENTPATH; ?>/foo-drawer/js/cvalidate.js"></script>
+
+</body>
+</html>
